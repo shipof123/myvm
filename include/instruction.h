1155613fde70078350a9e1bd1c74c3bea3913d1f
shipof123 <shipof123main@gmail.com>
2019-01-04 06:58:52 +0000

    Changing registry

--- include/instruction.h
+++ include/instruction.h
@@ -12,6 +12,8 @@ namespace myvm {
 		std::string name;
 		uint64_t    value;
 
+		void register_to_map();
+
 		operator int() {
 			return this->value;
 		}
@@ -19,10 +21,12 @@ namespace myvm {
 
 
 	struct Instruction {
+		std::string name;
 		uint64_t opcode;
+
 		std::function<void(Register&, const uint64_t)> func;
 
-		void register_to_map(const std::string& name);
+		void register_to_map();
 
 		void operator()(Register& r, const uint64_t v){
 			func(r , v);
@@ -37,9 +41,14 @@ namespace myvm {
 
 
 	std::map <std::string, Instruction> instruction_map;
+	std::map <std::string, Register>    register_map;
 
-	void Instruction::register_to_map(const std::string& name) {
-		instruction_map[name] = *this;
+	void Instruction::register_to_map() {
+		instruction_map[this->name] = *this;
+	}
+
+	void Register::register_to_map() {
+		register_map[this->name] = *this;
 	}
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 
 #include <iostream>
 
+
 int main(){
 	myvm::Register rax = {"rax", 10};
 	myvm::Register rdi = {"rdi", 5};
 
-	myvm::Instruction add{ 0x1 , myvm::instruction::add};
+	myvm::Instruction add{"add", 0x1 , myvm::instruction::add};
 
-	add.register_to_map("add");
+	add.register_to_map();
 
 	add(rax, rdi);
 
